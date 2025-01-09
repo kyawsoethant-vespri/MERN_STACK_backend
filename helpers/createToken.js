cc3fc8a90210d0fca3af74a911016bf91996381a
kyaw soe thant <kyawsoethant.vespri@gmail.com>
2025-01-09 10:54:58 +0630

    [Enhancement] refcator env file and create token file

--- helpers/createToken.js
+++ helpers/createToken.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const maxDay = 24 * 60 * 60; //1 day
 
 const createToken = (_id) => {
-    return jwt.sign({_id}, "secretary", {expiresIn: maxDay});
+    return jwt.sign({_id}, process.env.JWT_SECRET, {expiresIn: maxDay});
 };
 
 module.exports = createToken;
